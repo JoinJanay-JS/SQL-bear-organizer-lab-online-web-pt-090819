abb0c6ae6ad53e76208e6114aa1f5cd0d0eeb2eb
Janay Bailey <tjanay87@gmail.com>
2019-11-26 15:05:11 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", male, brown, happy, true);
+INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+     VALUES ("Rowdy", male, green, rowdy, false);
+INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+     VALUES ("Tabitha", female, red, silent, true);
+INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+     VALUES ("Sergeant Brown", male, tan, , true);
+INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+     VALUES ("Melissa", female, purple, sleepy, true);
+INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+     VALUES ("Grinch", male, green, angery, true);
+INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+     VALUES ("Wendy", female, turquoise, happy, female, true);
+INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+     VALUES (null, male, brown, happy, true);
