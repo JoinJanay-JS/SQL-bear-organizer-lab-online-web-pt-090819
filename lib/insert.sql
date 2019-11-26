20788863926b6ec90ea0d15fe0fda956283ad60b
Janay Bailey <tjanay87@gmail.com>
2019-11-26 16:53:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 15, male, brown, happy, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, male, green, rowdy, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Tabitha", 21, female, red, silent, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, male, tan, sad, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Melissa", 19, female, purple, sleepy, 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Grinch", 55, male, green, angry, 0); 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Wendy", 9, female, turquoise, happy, female, 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (null, 32, male, brown, happy, 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Black", "Cuddly", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Brown", "Crazy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabita", 8, "F", "Grizzly", "Kind", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Brown", "Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "Black", "Loud", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "M", "Grizzly", "Loving", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Black", "Sluggish", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 16, "M", "Grizzly", "Hungry", 0);
