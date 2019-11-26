34c0929099df7e3bb4341d3187408d4cbd215798
Janay Bailey <tjanay87@gmail.com>
2019-11-26 15:08:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
     VALUES ("Mr. Chocolate", 15, male, brown, happy, true);
 INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
-     VALUES ("Rowdy", male,  green, rowdy, false);
+     VALUES ("Rowdy", 5, male,  green, rowdy, false);
 INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
      VALUES ("Tabitha", 21, female,  red, silent, true);
 INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
-     VALUES ("Sergeant Brown", male, tan, , true);
+     VALUES ("Sergeant Brown", 2, male, tan, , true);
 INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
-     VALUES ("Melissa", female, purple, sleepy, true);
+     VALUES ("Melissa", 19, female, purple, sleepy, true);
 INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
-     VALUES ("Grinch", male, green, angery, true);
+     VALUES ("Grinch", 55, male, green, angery, true);
 INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
-     VALUES ("Wendy", female, turquoise, happy, female, true);
+     VALUES ("Wendy", 9, female, turquoise, happy, female, true);
 INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
-     VALUES (null, male, brown, happy, true);
+     VALUES (null, 32, male, brown, happy, true);
