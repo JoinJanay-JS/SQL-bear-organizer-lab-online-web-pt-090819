2d199f3d1cf3923478c4b6f4879a11599067b080
Janay Bailey <tjanay87@gmail.com>
2019-11-26 16:34:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
     VALUES ("Mr. Chocolate", 15, male, brown, happy, true);
-INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
      VALUES ("Rowdy", 5, male,  green, rowdy, false);
-INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
      VALUES ("Tabitha", 21, female,  red, silent, true);
-INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
      VALUES ("Sergeant Brown", 2, male, tan, , true);
-INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
      VALUES ("Melissa", 19, female, purple, sleepy, true);
-INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
      VALUES ("Grinch", 55, male, green, angery, true);
-INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
      VALUES ("Wendy", 9, female, turquoise, happy, female, true);
-INSERT INTO sql_queries (id, name, age, gender, color, temperament, alive)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
      VALUES (null, 32, male, brown, happy, true);
